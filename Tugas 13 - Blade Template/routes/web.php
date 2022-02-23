14b8f90b4df413b6fe9a749cc335d0f296a43c99
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-02-23 23:12:40 +0700

    Tugas 13 Blade Template

---
+++ Tugas 13 - Blade Template/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','HomeController@index');
+Route::get('/register','AuthController@register');
+Route::post('/kirim','AuthController@kirim');
+Route::view('/data-table','table.data-table');
+Route::view('/table','table.table');
+// Route::view('/admin2','layouts.admin');
+// Route::view('/register','register');
+// Route::view('/welcome','welcome');
+// Route::get('/','');
